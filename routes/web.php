cec9607e03ea3a23b0e02d7de5ddf0a45964a267
VentGrey <omarpurataf@gmail.com>
2021-01-29 16:53:54 -0600

    New laravel breeze build assets

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/','App\Http\Controllers\PagesController@index');
-Route::get('/about','App\Http\Controllers\PagesController@about');
-Route::get('/services','App\Http\Controllers\PagesController@services');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
