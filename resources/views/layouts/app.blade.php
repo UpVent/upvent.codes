8683ffc9e39e60adaa53fbc0c126da1b0a6c4729
VentGrey <omarpurataf@gmail.com>
2021-01-29 15:30:05 -0600

    Corrected extra semicolon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,6 @@
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     </head>
     <body class="antialiased">
-        @yield('content');
+        @yield('content')
     </body>
 </html>
