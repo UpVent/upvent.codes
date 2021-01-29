a5abaa6d0feda9dee14eb757f88faa7826368f14
VentGrey <omarpurataf@gmail.com>
2021-01-28 21:09:28 -0600

    Add new routes to web.php

--- routes/web.php
+++ routes/web.php
@@ -13,15 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-///////////////////////////////////
-// Route::get('/', function () { //
-//     return view('welcome');   //
-// });                           //
-///////////////////////////////////
-
 
 Route::get('/','App\Http\Controllers\PagesController@index');
-
-Route::get('/about', function () {
-    return view('pages.about');
-});
+Route::get('/about','App\Http\Controllers\PagesController@about');
+Route::get('/services','App\Http\Controllers\PagesController@services');
