be2ede751154507122feba579d327f0e7550b328
VentGrey <omarpurataf@gmail.com>
2021-01-29 15:06:26 -0600

    Used templating

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,20 +1,10 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>{{config('app.name')}} - Soluciones en la nube para tu negocio. Low Cost, siempre listas.</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-    </head>
-    <body class="antialiased">
-        <h1>
-            Nube Inteligente.
-            <br />
-            Para Negocios Inteligentes
-        </h1>
-        <p>Desarrollamos soluciones web inteligentes para cualquier tipo de negocio.</p>
-    </body>
-</html>
+@section('content')
+<h1>
+    Nube Inteligente.
+    <br />
+    Para Negocios Inteligentes
+</h1>
+<p>Desarrollamos soluciones web inteligentes para cualquier tipo de negocio.</p>
+@endsection
