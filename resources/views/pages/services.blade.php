63117093eea352278b1102285d1391b0e4179b05
VentGrey <omarpurataf@gmail.com>
2021-01-29 15:10:15 -0600

    Applied templating into page

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -1,18 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>{{config('app.name')}} - Soluciones en la nube para tu negocio. LowCost, siempre listas.</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-    </head>
-    <body class="antialiased">
-        <h1>
-            Servicios
-        </h1>
-        <p>Conozca la amplia variedad de servicios que UpVent puede ofrecerle a usted y a su empresa.</p>
-    </body>
-</html>
+@section('content')
+<h1>
+    Servicios
+</h1>
+<p>Conozca la amplia variedad de servicios que UpVent puede ofrecerle a usted y a su empresa.</p>
+@endsection
