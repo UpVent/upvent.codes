fdbc39f3b97e5c6a4deb9dbc94fc69f7773fa42c
VentGrey <omarpurataf@gmail.com>
2021-01-28 19:01:19 -0600

    Begin working on index page for upvent.codes site

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+///////////////////////////////////
+// Route::get('/', function () { //
+//     return view('welcome');   //
+// });                           //
+///////////////////////////////////
+
+
+Route::get('/','App\Http\Controllers\PagesController@index');
 
 Route::get('/about', function () {
     return view('pages.about');
