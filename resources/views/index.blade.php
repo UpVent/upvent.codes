7dd6af941b77287342741a82abf81b8d4fe51466
VentGrey <omarpurataf@gmail.com>
2021-01-30 17:25:37 -0600

    Added favicon + navbar improvement

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,10 +16,9 @@
 
     </head>
     <body class="antialiased">
-
         <!-- This example requires Tailwind CSS v2.0+ -->
         <div>
-            <nav class="bg-upvent">
+            <nav class="bg-upvent shadow-inner">
                 <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div class="flex items-center justify-between h-16">
                         <div class="flex items-center">
