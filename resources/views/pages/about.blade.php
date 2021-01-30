7ad62d14006562443958e5d5164a82b7089ca332
VentGrey <omarpurataf@gmail.com>
2021-01-29 18:27:19 -0600

    Wiped out, couldnt fix laravel breeze shithole

--- resources/views/pages/about.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<h1>
-    {{$title}}
-</h1>
-<p>Llevando empresas a la nube desde nuestros inicios.</p>
-<p>UpVent es una compañía de tecnología y software empresarial que sigue el modelo del software libre enfocado a las micro, pequeñas y medianas empresas Mexicanas.</p>
-@endsection
