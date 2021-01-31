236a4e015af1da2fe2f9ad450a696e1d213191df
VentGrey <omarpurataf@gmail.com>
2021-01-31 17:15:19 -0600

    Modifications on index navbar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,66 +20,70 @@
     <body class="antialiased">
         <!-- This example requires Tailwind CSS v2.0+ -->
         <div>
-            <nav class="bg-upvent shadow-inner">
-                <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                    <div class="flex items-center justify-between h-16">
-                        <div class="flex items-center">
-                            <div class="flex-shrink-0">
-                                <img class="h-8 w-13 object-fill" src="{{asset('img/logo-white.png')}}" alt="Workflow">
+            <nav class="flex items-center justify-between flex-wrap bg-upvent shadow-inner p-6">
+                <!-- Navbar Logo -->
+                <div class="flex items-center flex-no-shrink mr-6">
+                    <img class="object-center object-contain h-10 w-15" src="{{asset('img/logo-white.png')}}" alt="UpVent White Logo" />
+                </div>
+                <!-- Menu hidden -->
+                <div class="block lg:hidden">
+                    <button data-toggle-hide="[data-nav-content]" class="flex items-center px-3 py-2 border rounded text-white border-teal-light hover:text-white hover:border-white rounded focus:outline-none focus:shadow-outline">
+                        <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <title>
+                                Menu
+                            </title>
+                            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
+                        </svg>
+                    </button>
+                </div>
+                <!-- Menu shown -->
+                <div data-nav-content="" class="w-full block flex-grow lg:flex lg:items-center lg:w-auto hidden lg:block">
+                    <div class="text-sm text-white lg:flex-grow">
+                        <a class="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4 rounded focus:outline-none focus:shadow-outline" href="#responsive-header">
+                            Inicio
+                        </a>
+
+                        <a href="#responsive-header" class=" block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4 rounded focus:outline-none focus:shadow-outline">
+                            Blog
+                        </a>
+
+                        <a href="#responsive-header" class=" block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4 rounded focus:outline-none focus:shadow-outline">
+                            Nosotros
+                        </a>     <a href="#responsive-header" class=" block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4 rounded focus:outline-none focus:shadow-outline">
+                            Servicios
+                        </a>     <a href="#responsive-header" class=" block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4 rounded focus:outline-none focus:shadow-outline">
+                            Contacto
+                        </a>     <a href="#responsive-header" class=" block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4 rounded focus:outline-none focus:shadow-outline">
+                            Marketcloud
+                        </a>
+                    </div>
+                    <div class="text-sm text-white lg:flex-shrink">
+
+
+                        @if (Route::has('login'))
+                            <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                                @auth
+                                <a href="{{ url('/dashboard') }}" class="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4 rounded focus:outline-none focus:shadow-outline">Dashboard</a>
+                        @else
+                                <a href="{{ route('login') }}" class="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4 rounded focus:outline-none focus:shadow-outline">
+                                    Login
+                                </a>
+                                @if (Route::has('register'))
+                                    <a href="{{ route('register') }}" class="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4 rounded focus:outline-none focus:shadow-outline">Register</a>
+                                @endif
+                                @endauth
                             </div>
-                            <div class="hidden sm:block">
-                                <div class="ml-10 flex items-baseline space-x-4">
-                                    <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                                    <a href="#" class="shadow-xl bg-white text-purple px-3 py-2 rounded-md text-sm font-medium">Inicio</a>
-
-                                    <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Blog</a>
-
-                                    <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Nosotros</a>
-
-                                    <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Servicios</a>
-
-                                    <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contacto</a>
-
-                                    <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Marketcloud</a>
-                                </div>
-                            </div>
-
-
-                            @if (Route::has('login'))
-                                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                                    @auth
-                                    <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
-                            @else
-                                    <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
-
-                                    @if (Route::has('register'))
-                                        <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
-                                    @endif
-                                    @endauth
-                                </div>
-                            @endif
-
-                            <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-                                <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
-
-                                </div>
-                            </div>
-
-                        </div>
+                        @endif
                     </div>
                 </div>
             </nav>
-
-            <main>
-                <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-                    <!-- Replace with your content -->
-                    <div class="px-4 py-6 sm:px-0">
-                        <div class="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
-                    </div>
-                    <!-- /End replace -->
-                </div>
-            </main>
         </div>
 
+
+
+
+
+        <!-- Scripts -->
+        <script src="/js/app.js" type="text/javascript"></script>
     </body>
 </html>
