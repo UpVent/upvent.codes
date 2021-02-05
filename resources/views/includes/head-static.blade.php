7951f06f8bc93b548b5d870962bb510f0b020770
Omar Jair Purata Funes <omarpurataf@gmail.com>
2021-02-04 21:06:02 -0600

    Separated head includes
    This makes common imports like css, icons and fonts easier to use in case other files are needed.

---
+++ resources/views/includes/head-static.blade.php
@@ -0,0 +1,11 @@
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Roboto&display=swap" rel="stylesheet">
+
+        <!-- Favicon -->
+        <link rel="shortcut icon" href="{{ asset('img/favicon.ico') }}">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="/css/app.css" type="text/css" media="screen" />
+        <link rel="stylesheet" href="{{ asset('css/index.css') }}" type="text/css" media="screen" />
