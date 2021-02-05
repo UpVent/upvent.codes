53ab587734a1043015e2abae881830180275b14f
Omar Jair Purata Funes <omarpurataf@gmail.com>
2021-02-04 19:26:46 -0600

    Finished footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,116 +1,96 @@
-<footer class="bg-gray-800 pt-10 sm:mt-10 pt-10">
+<footer class="bg-upvent pt-10 sm:mt-10 pt-10">
     <div class="max-w-6xl m-auto text-gray-800 flex flex-wrap justify-left">
         <!-- Col-1 -->
         <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
-            <!-- Col Title -->
-            <div class="text-xs uppercase text-gray-400 font-medium mb-6">
-                Getting Started
+            <!-- Logo -->
+            <div class="container m-2">
+                <img class="object-contain m-2" alt="" src="{{asset('img/logo-white.png')}}"/>
+                <p class="font-bold text-white m-1">
+                    Todos los derechos reservados © - UpVent @php echo date("Y"); @endphp. Todos los logos son marcas registradas de sus respectivos dueños.
+                </p>
             </div>
-
-            <!-- Links -->
-            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
-                Installation
-            </a>
-            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
-                Release Notes
-            </a>
-            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
-                Upgrade Guide
-            </a>
-            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
-                Using with Preprocessors
-            </a>
-            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
-                Optimizing for Production
-            </a>
-            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
-                Browser Support
-            </a>
-            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
-                IntelliSense
-            </a>
         </div>
 
         <!-- Col-2 -->
         <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
             <!-- Col Title -->
-            <div class="text-xs uppercase text-gray-400 font-medium mb-6">
-                Core Concepts
+            <div class="text-xl uppercase text-white font-medium mb-6">
+                Enlaces Rápidos
             </div>
 
             <!-- Links -->
-            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
-                Utility-First
+            <a href="#" class="my-3 block text-white hover:text-gray-100 text-sm font-medium duration-700">
+                <i class="bi bi-record-fill"></i>    Inicio
             </a>
-            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
-                Responsive Design
+            <a href="#" class="my-3 block text-white hover:text-gray-100 text-sm font-medium duration-700">
+                <i class="bi bi-record-fill"></i>    Blog
             </a>
-            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
-                Hover, Focus, & Other States
+            <a href="#" class="my-3 block text-white hover:text-gray-100 text-sm font-medium duration-700">
+                <i class="bi bi-record-fill"></i>    Nosotros
             </a>
-            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
-                Dark Mode
+            <a href="#" class="my-3 block text-white hover:text-gray-100 text-sm font-medium duration-700">
+                <i class="bi bi-record-fill"></i>    Servicios
             </a>
-            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
-                Adding Base Styles
+            <a href="#" class="my-3 block text-white hover:text-gray-100 text-sm font-medium duration-700">
+                <i class="bi bi-record-fill"></i>    Contacto
             </a>
-            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
-                Extracting Components
+            <a href="#" class="my-3 block text-white hover:text-gray-100 text-sm font-medium duration-700">
+                <i class="bi bi-record-fill"></i>    Marketcloud
             </a>
-            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
-                Adding New Utilities
+            <a href="#" class="my-3 block text-white hover:text-gray-100 text-sm font-medium duration-700">
+                <i class="bi bi-record-fill"></i>    Código Abierto
             </a>
         </div>
 
         <!-- Col-3 -->
         <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
             <!-- Col Title -->
-            <div class="text-xs uppercase text-gray-400 font-medium mb-6">
-                Customization
+            <div class="text-xl uppercase text-white font-medium mb-6">
+                Enlaces Importantes
             </div>
 
             <!-- Links -->
-            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
-                Configuration
+            <a href="#" class="my-3 block text-white hover:text-gray-100 text-sm font-medium duration-700">
+                <i class="bi bi-record-fill"></i>  Términos y Condiciones
             </a>
-            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
-                Theme Configuration
+            <a href="#" class="my-3 block text-white hover:text-gray-100 text-sm font-medium duration-700">
+                <i class="bi bi-record-fill"></i>  Política de Privacidad
             </a>
-            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
-                Breakpoints
+            <a href="#" class="my-3 block text-white hover:text-gray-100 text-sm font-medium duration-700">
+                <i class="bi bi-record-fill"></i> Repositorio de código libre
             </a>
-            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
-                Customizing Colors
+            <a href="#" class="my-3 block text-white hover:text-gray-100 text-sm font-medium duration-700">
+                <i class="bi bi-record-fill"></i> Licencias
             </a>
-            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
-                Customizing Spacing
+            <a href="#" class="my-3 block text-white hover:text-gray-100 text-sm font-medium duration-700">
+                <i class="bi bi-record-fill"></i> Equipo
             </a>
-            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
-                Configuring Variants
+            <a href="#" class="my-3 block text-white hover:text-gray-100 text-sm font-medium duration-700">
+                <i class="bi bi-record-fill"></i> Eventos
             </a>
-            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
-                Plugins
+            <a href="#" class="my-3 block text-white hover:text-gray-100 text-sm font-medium duration-700">
+                <i class="bi bi-record-fill"></i>  Aliados
             </a>
         </div>
 
         <!-- Col-3 -->
         <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
             <!-- Col Title -->
-            <div class="text-xs uppercase text-gray-400 font-medium mb-6">
-                Community
+            <div class="text-xl uppercase text-white font-medium mb-6">
+                Comunidad
             </div>
 
             <!-- Links -->
-            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
+            <a href="#" class="my-3 block text-white hover:text-gray-100 text-sm font-medium duration-700">
                 GitHub
             </a>
-            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
+            <a href="#" class="my-3 block text-white hover:text-gray-100 text-sm font-medium duration-700">
                 Discord
             </a>
-            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
+            <a href="#" class="my-3 block text-white hover:text-gray-100 text-sm font-medium duration-700">
                 Twitter
             </a>
-            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
+            <a href="#" class="my-3 block text-white hover:text-gray-100 text-sm font-medium duration-700">
                 YouTube
             </a>
         </div>
@@ -119,28 +99,28 @@
     <!-- Copyright Bar -->
     <div class="pt-2">
         <div class="flex pb-5 px-3 m-auto pt-5
-            border-t border-gray-500 text-gray-400 text-sm
+            border-t border-white-500 text-white text-sm
             flex-col md:flex-row max-w-6xl">
             <div class="mt-2">
-                © Copyright 1998-year. All Rights Reserved.
+                © Copyright 2020-@php echo date("Y"); @endphp. All Rights Reserved.
             </div>
 
             <!-- Required Unicons (if you want) -->
-            <div class="md:flex-auto md:flex-row-reverse mt-2 flex-row flex">
+            <div class="text-white md:flex-auto md:flex-row-reverse mt-2 flex-row flex">
                 <a href="#" class="w-6 mx-1">
-                    <i class="uil uil-facebook-f"></i>
+                    <i class="bi bi-facebook"></i>
                 </a>
                 <a href="#" class="w-6 mx-1">
-                    <i class="uil uil-twitter-alt"></i>
+                    <i class="bi bi-twitter"></i>
                 </a>
                 <a href="#" class="w-6 mx-1">
-                    <i class="uil uil-youtube"></i>
+                    <i class="bi bi-github"></i>
                 </a>
                 <a href="#" class="w-6 mx-1">
-                    <i class="uil uil-linkedin"></i>
+                    <i class="bi bi-envelope-fill"></i>
                 </a>
                 <a href="#" class="w-6 mx-1">
-                    <i class="uil uil-instagram"></i>
+                    <i class="bi bi-instagram"></i>
                 </a>
             </div>
         </div>
