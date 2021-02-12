327a0d5b20c760d0e63ebab43cd9a5342559cab7
VentGrey <omarpurataf@gmail.com>
2021-02-11 21:30:13 -0600

    Added new component

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -402,88 +402,92 @@
 
                     <!-- Sección de Beneficios -->
 
-                    <div class="lg:text-center">
-                        <p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-                            Beneficios.
-                        </p>
-                        <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
-                            Existe un gran número de razones para elegir a UpVent como solución. Le presentamos orgullosamente las mejores.
-                        </p>
+                    <div class="max-w-screen-xl p-4 bg-white dark:bg-gray-800 mx-auto px-4 sm:px-6 lg:px-8 relative py-26 lg:mt-20">
+                        <div class="relative">
+                            <div class="lg:grid lg:grid-flow-row-dense lg:grid-cols-2 lg:gap-8 lg:items-center">
+                                <div class="lg:col-start-2 lg:max-w-2xl ml-auto">
+                                    <p class="text-base leading-6 text-indigo-500 font-semibold uppercase">
+                                        Interactive
+                                    </p>
+                                    <h4 class="mt-2 text-2xl leading-8 font-extrabold text-gray-900 dark:text-white sm:text-3xl sm:leading-9">
+                                        Interactivity between team members is the key of the success.
+                                    </h4>
+                                    <p class="mt-4 text-lg leading-6 text-gray-500 dark:text-gray-300">
+                                        Build a simply and powered collaborative space for all your team. Track, share, measure, you have a fully control, it&#x27;s never be simply and efficient.
+                                    </p>
+                                    <ul class="mt-8 md:grid md:grid-cols-2 gap-6">
+                                        <li class="mt-6 lg:mt-0">
+                                            <div class="flex">
+                                                <span class="flex-shrink-0 flex items-center justify-center h-6 w-6 rounded-full bg-green-100 text-green-800 dark:text-green-500 drark:bg-transparent">
+                                                    <svg class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+                                                        <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd">
+                                                        </path>
+                                                    </svg>
+                                                </span>
+                                                <span class="ml-4 text-base leading-6 font-medium text-gray-500 dark:text-gray-200">
+                                                    Live modifications
+                                                </span>
+                                            </div>
+                                        </li>
+                                        <li class="mt-6 lg:mt-0">
+                                            <div class="flex">
+                                                <span class="flex-shrink-0 flex items-center justify-center h-6 w-6 rounded-full bg-green-100 text-green-800 dark:text-green-500 drark:bg-transparent">
+                                                    <svg class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+                                                        <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd">
+                                                        </path>
+                                                    </svg>
+                                                </span>
+                                                <span class="ml-4 text-base leading-6 font-medium text-gray-500 dark:text-gray-200">
+                                                    Data tracker
+                                                </span>
+                                            </div>
+                                        </li>
+                                        <li class="mt-6 lg:mt-0">
+                                            <div class="flex">
+                                                <span class="flex-shrink-0 flex items-center justify-center h-6 w-6 rounded-full bg-green-100 text-green-800 dark:text-green-500 drark:bg-transparent">
+                                                    <svg class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+                                                        <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd">
+                                                        </path>
+                                                    </svg>
+                                                </span>
+                                                <span class="ml-4 text-base leading-6 font-medium text-gray-500 dark:text-gray-200">
+                                                    24/24 support
+                                                </span>
+                                            </div>
+                                        </li>
+                                        <li class="mt-6 lg:mt-0">
+                                            <div class="flex">
+                                                <span class="flex-shrink-0 flex items-center justify-center h-6 w-6 rounded-full bg-green-100 text-green-800 dark:text-green-500 drark:bg-transparent">
+                                                    <svg class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+                                                        <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd">
+                                                        </path>
+                                                    </svg>
+                                                </span>
+                                                <span class="ml-4 text-base leading-6 font-medium text-gray-500 dark:text-gray-200">
+                                                    Free tips to improve work time
+                                                </span>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="mt-10 lg:-mx-4 relative relative-20 lg:mt-0 lg:col-start-1">
+                                    <div class="relative space-y-4">
+                                        <div class="flex items-end justify-center lg:justify-start space-x-4">
+                                            <img class="rounded-lg shadow-lg w-32 md:w-56" width="200" src="/images/object/8.jpg" alt="1"/>
+                                            <img class="rounded-lg shadow-lg w-40 md:w-64" width="260" src="/images/landscape/4.jpg" alt="2"/>
+                                        </div>
+                                        <div class="flex items-start justify-center lg:justify-start space-x-4 ml-12">
+                                            <img class="rounded-lg shadow-lg w-24 md:w-40" width="170" src="/images/landscape/3.jpg" alt="3"/>
+                                            <img class="rounded-lg shadow-lg w-32 md:w-56" width="200" src="/images/object/9.jpg" alt="4"/>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
 
-                    <div class="container mx-auto mt-8 grid grid-cols-4 gap-2">
-                        <div>
-                            <div class="shadow-lg rounded-2xl w-64 p-4 bg-white relative overflow-hidden">
-                                <img alt="moto" src="/images/object/1.png" class="absolute -right-20 -bottom-8 h-40 w-40 mb-4"/>
-                                <div class="w-4/6">
-                                    <p class="text-gray-800 text-lg font-medium mb-2">
-                                        Low Cost
-                                    </p>
-                                    <p class="text-gray-400 text-xs">
-                                        Detail is not an obsession, it is the very essence of perfection.
-                                    </p>
-                                    <p class="text-indigo-500 text-xl font-medium">
-                                        $399
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
 
 
-                        <div>
-                            <div class="shadow-lg rounded-2xl w-64 p-4 bg-white relative overflow-hidden">
-                                <img alt="moto" src="/images/object/1.png" class="absolute -right-20 -bottom-8 h-40 w-40 mb-4"/>
-                                <div class="w-4/6">
-                                    <p class="text-gray-800 text-lg font-medium mb-2">
-                                        Libre y Open Source
-                                    </p>
-                                    <p class="text-gray-400 text-xs">
-                                        Detail is not an obsession, it is the very essence of perfection.
-                                    </p>
-                                    <p class="text-indigo-500 text-xl font-medium">
-                                        $399
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-
-
-                        <div>
-                            <div class="shadow-lg rounded-2xl w-64 p-4 bg-white relative overflow-hidden">
-                                <img alt="moto" src="/images/object/1.png" class="absolute -right-20 -bottom-8 h-40 w-40 mb-4"/>
-                                <div class="w-4/6">
-                                    <p class="text-gray-800 text-lg font-medium mb-2">
-                                        Educativo
-                                    </p>
-                                    <p class="text-gray-400 text-xs">
-                                        Detail is not an obsession, it is the very essence of perfection.
-                                    </p>
-                                    <p class="text-indigo-500 text-xl font-medium">
-                                        $399
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-
-
-                        <div>
-                            <div class="shadow-lg rounded-2xl w-64 p-4 bg-white relative overflow-hidden">
-                                <img alt="moto" src="/images/object/1.png" class="absolute -right-20 -bottom-8 h-40 w-40 mb-4"/>
-                                <div class="w-4/6">
-                                    <p class="text-gray-800 text-lg font-medium mb-2">
-                                        Innovador
-                                    </p>
-                                    <p class="text-gray-400 text-xs">
-                                        Detail is not an obsession, it is the very essence of perfection.
-                                    </p>
-                                    <p class="text-indigo-500 text-xl font-medium">
-                                        $399
-                                    </p>
-                                </div>
-                            </div></div>
-
-
-                    </div>
 
                     <!-- Footer -->
                     @include('includes.footer')
