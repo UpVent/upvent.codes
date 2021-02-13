ef29dc9504cb07d8486264542751fe2eb890155d
VentGrey <omarpurataf@gmail.com>
2021-02-13 17:15:39 -0600

    Finished index (for now)

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -409,65 +409,68 @@
                             <div class="lg:grid lg:grid-flow-row-dense lg:grid-cols-2 lg:gap-8 lg:items-center">
                                 <div class="lg:col-start-2 lg:max-w-2xl ml-auto">
                                     <h4 class="mt-2 text-2xl leading-8 font-extrabold text-gray-900 dark:text-white sm:text-3xl sm:leading-9">
-                                        Interactivity between team members is the key of the success.
+                                        Beneficios con UpVent
                                     </h4>
                                     <p class="mt-4 text-lg leading-6 text-gray-500 dark:text-gray-300">
-                                        Build a simply and powered collaborative space for all your team. Track, share, measure, you have a fully control, it&#x27;s never be simply and efficient.
+                                        Hay un gran número de razones por las cuales UpVent es una empresa de calidad y grandes valores. Aquí les presentaremos las más importantes.
                                     </p>
                                     <ul class="mt-8 md:grid md:grid-cols-2 gap-6">
                                         <li class="mt-6 lg:mt-0">
                                             <div class="flex">
-                                                <span class="flex-shrink-0 flex items-center justify-center h-6 w-6 rounded-full bg-green-100 text-green-800 dark:text-green-500 drark:bg-transparent">
+                                                <span class="flex-shrink-0 flex items-center justify-center h-6 w-6 rounded-full bg-blue-100 text-green-800 dark:text-green-500 drark:bg-transparent">
                                                     <svg class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
                                                         <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd">
                                                         </path>
                                                     </svg>
                                                 </span>
                                                 <span class="ml-4 text-base leading-6 font-medium text-gray-500 dark:text-gray-200">
-                                                    Live modifications
+                                                    Low Cost
                                                 </span>
                                             </div>
                                         </li>
                                         <li class="mt-6 lg:mt-0">
                                             <div class="flex">
-                                                <span class="flex-shrink-0 flex items-center justify-center h-6 w-6 rounded-full bg-green-100 text-green-800 dark:text-green-500 drark:bg-transparent">
+                                                <span class="flex-shrink-0 flex items-center justify-center h-6 w-6 rounded-full bg-blue-100 text-green-800 dark:text-green-500 drark:bg-transparent">
                                                     <svg class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
                                                         <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd">
                                                         </path>
                                                     </svg>
                                                 </span>
                                                 <span class="ml-4 text-base leading-6 font-medium text-gray-500 dark:text-gray-200">
-                                                    Data tracker
+                                                    Libre y Open Source
                                                 </span>
                                             </div>
                                         </li>
                                         <li class="mt-6 lg:mt-0">
                                             <div class="flex">
-                                                <span class="flex-shrink-0 flex items-center justify-center h-6 w-6 rounded-full bg-green-100 text-green-800 dark:text-green-500 drark:bg-transparent">
+                                                <span class="flex-shrink-0 flex items-center justify-center h-6 w-6 rounded-full bg-blue-100 text-green-800 dark:text-green-500 drark:bg-transparent">
                                                     <svg class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
                                                         <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd">
                                                         </path>
                                                     </svg>
                                                 </span>
                                                 <span class="ml-4 text-base leading-6 font-medium text-gray-500 dark:text-gray-200">
-                                                    24/24 support
+                                                    Educativo
                                                 </span>
                                             </div>
                                         </li>
                                         <li class="mt-6 lg:mt-0">
                                             <div class="flex">
-                                                <span class="flex-shrink-0 flex items-center justify-center h-6 w-6 rounded-full bg-green-100 text-green-800 dark:text-green-500 drark:bg-transparent">
+                                                <span class="flex-shrink-0 flex items-center justify-center h-6 w-6 rounded-full bg-blue-100 text-green-800 dark:text-green-500 drark:bg-transparent">
                                                     <svg class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
                                                         <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd">
                                                         </path>
                                                     </svg>
                                                 </span>
                                                 <span class="ml-4 text-base leading-6 font-medium text-gray-500 dark:text-gray-200">
-                                                    Free tips to improve work time
+                                                    Innovador
                                                 </span>
                                             </div>
                                         </li>
                                     </ul>
+                                    <div class="m-8 p-4 container mx-auto">
+                                        <a class="p-4 bg-upvent hover:bg-blue-600 text-white font-bold border-black rounded-lg shadow-lg">Comenzar con UpVent <i class="ml-2 mr-2 mb-2 bi bi-trophy-fill"></i></a>
+                                    </div>
                                 </div>
                                 <div class="mt-10 lg:-mx-4 relative relative-20 lg:mt-0 lg:col-start-1">
                                     <div class="relative space-y-4">
