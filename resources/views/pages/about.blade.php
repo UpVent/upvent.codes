5de905671e9273998fdfb569b11d07ad26e0b7d8
VentGrey <omarpurataf@gmail.com>
2021-02-14 01:02:41 -0600

    Started working on about page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -4,14 +4,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{config('app.name', 'UpVent')}} - Soluciones en la nube para tu negocio. Low Cost, Siempre Listas.</title>
-
+        <title>{{config('app.name', 'UpVent')}} - Acerca De.</title>
 
         @include('includes.head-static')
 
     </head>
     <body class="antialiased font-montserrat">
-        <!-- This example requires Tailwind CSS v2.0+ -->
         <div class="shadow-xl">
             <nav class=" flex items-center justify-between flex-wrap bg-upvent p-6">
                 <!-- Navbar Logo -->
@@ -85,9 +83,6 @@
             </nav>
         </div>
 
-        <!-- Beta banner -->
-        @include('includes.notice-banner')
-
         <!-- Jumbotron -->
         <div class="relative bg-white overflow-hidden">
             <div class="max-w-7xl mx-auto">
@@ -95,34 +90,91 @@
                     <main class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
                         <div class="sm:text-center lg:text-center">
                             <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                                <span class="block xl:inline">Llevando empresas a la nube desde nuestros</span>
-                                <span class="block text-upvent xl:inline">inicios.</span>
+                                <span class="block text-upvent xl:inline">Nosotros</span>
                             </h1>
                             <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                                Líder Mexicano en desarrollo de soluciones empresariales de código libre.
+                                Llevando empresas a la nube desde nuestros inicios.
                             </p>
-                            <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-                                <div class="rounded-md shadow">
-                                    <a href="#" class="shadow-lg w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-upvent hover:bg-blue-800 md:py-4 md:text-lg md:px-10">
-                                        Conocer más  <i class="bi bi-forward px-1"></i>
-                                    </a>
-                                </div>
-                                <div class="mt-3 sm:mt-0 sm:ml-3">
-                                    <a href="#" class="shadow-lg w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-blue-700 bg-blue-100 hover:bg-blue-200 md:py-4 md:text-lg md:px-10">
-                                        Portafolio  <i class="bi bi-briefcase px-1"></i></i>
-                                    </a>
-                                </div>
-                            </div>
+
+                            <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                                UpVent es una compañía de tecnología y software empresarial que sigue el modelo del software libre enfocado a las micro, pequeñas y medianas empresas Mexicanas.
+                            </p>
                         </div>
                     </main>
                 </div>
             </div>
             <div class="sm:hidden md:inline-flex lg:inline-flex p-4 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-                <img class="rounded-lg object-center object-contain h-5/6 w-4/6" src="{{asset('img/coworkers.svg')}}" alt="">
+                <img class="rounded-lg object-center object-contain h-5/6 w-4/6" src="{{asset('img/about-us.svg')}}" alt="">
             </div>
         </div>
 
         <hr />
+
+        <!-- Values -->
+        <div class="sm:text-center md:text-center lg:text-center">
+            <p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                Nuestros valores, y algo más...
+            </p>
+            <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+                Conozca los tres pilares que hacen UpVent una realidad. Nuestros valores son fundamento de nuestra empresa y gracias a ellos podemos brindarle productos éticos en todo momento y en todo lugar.
+            </p>
+        </div>
+
+        <div class="sm:flex flex-wrap justify-center items-center text-center gap-4">
+            <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/4 px-4 py-4 bg-white mt-6  shadow-lg rounded-lg dark:bg-gray-800">
+                <div class="flex-shrink-0">
+                    <div class="flex items-center mx-auto justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                        <svg width="20" height="20" fill="currentColor" class="h-6 w-6" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
+                            </path>
+                        </svg>
+                    </div>
+                </div>
+                <h3 class="text-2xl sm:text-xl text-gray-700 font-semibold dark:text-white py-4">
+                    Website Design
+                </h3>
+                <p class="text-md  text-gray-500 dark:text-gray-300 py-4">
+                    Encompassing today’s website design technology to integrated and build solutions relevant to your business.
+                </p>
+            </div>
+
+            <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/4 px-4 py-4 mt-6 sm:mt-16 md:mt-20 lg:mt-24 bg-white shadow-lg rounded-lg dark:bg-gray-800">
+                <div class="flex-shrink-0">
+                    <div class="flex items-center mx-auto justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                        <svg width="20" height="20" fill="currentColor" class="h-6 w-6" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
+                            </path>
+                        </svg>
+                    </div>
+                </div>
+                <h3 class="text-2xl sm:text-xl text-gray-700 font-semibold dark:text-white py-4">
+                    Branding
+                </h3>
+                <p class="text-md text-gray-500 dark:text-gray-300 py-4">
+                    Share relevant, engaging, and inspirational brand messages to create a connection with your audience.
+                </p>
+            </div>
+
+            <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/4 mt-6  px-4 py-4 bg-white shadow-lg rounded-lg dark:bg-gray-800">
+                <div class="flex-shrink-0">
+                    <div class="flex items-center mx-auto justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                        <svg width="20" height="20" fill="currentColor" class="h-6 w-6" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
+                            </path>
+                        </svg>
+                    </div>
+                </div>
+                <h3 class="text-2xl sm:text-xl text-gray-700 font-semibold dark:text-white py-4">
+                    SEO Marketing
+                </h3>
+                <p class="text-md  text-gray-500 dark:text-gray-300 py-4">
+                    Let us help you level up your search engine game, explore our solutions for digital marketing for your business.
+                </p>
+            </div>
+
+        </div>
+
+
 
         <!-- Footer -->
         @include('includes.footer')
