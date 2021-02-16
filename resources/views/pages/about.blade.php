1649a8e74e122e2f47982371b3a517cde81da29d
VentGrey <omarpurataf@gmail.com>
2021-02-16 16:49:38 -0600

    Added values

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -123,52 +123,43 @@
         <div class="sm:flex flex-wrap justify-center items-center text-center gap-4">
             <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/4 px-4 py-4 bg-white mt-6  shadow-lg rounded-lg dark:bg-gray-800">
                 <div class="flex-shrink-0">
-                    <div class="flex items-center mx-auto justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                        <svg width="20" height="20" fill="currentColor" class="h-6 w-6" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
-                            </path>
-                        </svg>
+                    <div class="flex items-center mx-auto justify-center h-12 w-12 rounded-md bg-blue-500 text-white">
+                        <i class="bi bi-book-fill"></i>
                     </div>
                 </div>
                 <h3 class="text-2xl sm:text-xl text-gray-700 font-semibold dark:text-white py-4">
-                    Website Design
+                    Educación
                 </h3>
                 <p class="text-md  text-gray-500 dark:text-gray-300 py-4">
-                    Encompassing today’s website design technology to integrated and build solutions relevant to your business.
+                    Compartir el conocimiento es uno de los valores que fundó UpVent. Por ello procuramos liberar, compartir y escribir tutoriales actualizados de tecnología para retribuir a nuestra comunidad.
                 </p>
             </div>
 
             <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/4 px-4 py-4 mt-6 sm:mt-16 md:mt-20 lg:mt-24 bg-white shadow-lg rounded-lg dark:bg-gray-800">
                 <div class="flex-shrink-0">
-                    <div class="flex items-center mx-auto justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                        <svg width="20" height="20" fill="currentColor" class="h-6 w-6" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
-                            </path>
-                        </svg>
+                    <div class="flex items-center mx-auto justify-center h-12 w-12 rounded-md bg-blue-500 text-white">
+                        <i class="bi bi-graph-up"></i>
                     </div>
                 </div>
                 <h3 class="text-2xl sm:text-xl text-gray-700 font-semibold dark:text-white py-4">
-                    Branding
+                    Crecimiento
                 </h3>
                 <p class="text-md text-gray-500 dark:text-gray-300 py-4">
-                    Share relevant, engaging, and inspirational brand messages to create a connection with your audience.
+                    Al ser entusiastas de la cultura DevOps nos gusta construir software escalable, modular y manejable para nuestros clientes. Siempre escalable, siempre seguro, siempre eficiente.
                 </p>
             </div>
 
             <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/4 mt-6  px-4 py-4 bg-white shadow-lg rounded-lg dark:bg-gray-800">
                 <div class="flex-shrink-0">
-                    <div class="flex items-center mx-auto justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                        <svg width="20" height="20" fill="currentColor" class="h-6 w-6" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
-                            </path>
-                        </svg>
+                    <div class="flex items-center mx-auto justify-center h-12 w-12 rounded-md bg-blue-500 text-white">
+                        <i class="bi bi-star-fill"></i>
                     </div>
                 </div>
                 <h3 class="text-2xl sm:text-xl text-gray-700 font-semibold dark:text-white py-4">
-                    SEO Marketing
+                    Ética
                 </h3>
                 <p class="text-md  text-gray-500 dark:text-gray-300 py-4">
-                    Let us help you level up your search engine game, explore our solutions for digital marketing for your business.
+                    No vendemos, intercambiamos ni liberamos los datos de nuestros clientes. Toda tu información es confidencial y todo nuestro software vendido cumple con las licencias que respetan tu libertad.
                 </p>
             </div>
 
