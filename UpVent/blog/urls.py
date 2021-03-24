35eb72e0c2ef677039ecb7e02916d154659c1c6a
VentGrey <omarpurataf@gmail.com>
2021-03-24 01:15:14 -0600

    Added URL's .py

---
+++ UpVent/blog/urls.py
@@ -0,0 +1,10 @@
+from . import views
+from django.urls import path
+from .views import Blog, BlogEntry
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', Blog, name='home'),
+    path('<slug:slug>/', BlogEntry, name='Entry')
+]
