a990542edd5f436fd40500e463ac6300febe29a9
VentGrey <omarpurataf@gmail.com>
2021-03-24 01:15:36 -0600

    Modified model

--- UpVent/blog/models.py
+++ UpVent/blog/models.py
@@ -8,12 +8,13 @@ STATUS = (
 )
 
 class Post(models.Model):
-    title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
-    updated_on = models.DateTimeField(auto_now= True)
-    content = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(verbose_name="Título", max_length=200, unique=True)
+    category = models.CharField(verbose_name="Categoría", max_length=20)
+    slug = models.SlugField(verbose_name="Slug", max_length=200, unique=True)
+    author = models.ForeignKey(User, verbose_name="Autor", on_delete= models.CASCADE,related_name='blog_posts')
+    updated_on = models.DateTimeField(verbose_name="Actualizado el: ", auto_now= True)
+    content = models.TextField(verbose_name="Contenido")
+    created_on = models.DateTimeField(verbose_name="Creado el: ", auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
 
     class Meta:
