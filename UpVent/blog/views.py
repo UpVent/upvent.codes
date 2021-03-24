ca11de3bda2e777075df7f74232c660cf778d2d7
VentGrey <omarpurataf@gmail.com>
2021-03-24 01:15:02 -0600

    Added blog and posts

--- UpVent/blog/views.py
+++ UpVent/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Post
+
+def Blog(request):
+    posts = Post.objects.all()[::-1]
+    return render(request, 'blog/blog.html', {'posts':posts})
+
+def BlogEntry(request, article_id):
+    post = Post.objects.get(id=article_id)
+    return render(request, 'blog/article.html', {'post':post})
