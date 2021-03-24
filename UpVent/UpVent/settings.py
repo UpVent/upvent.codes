23a9f55ff202c2feb21b7c6336bb90ec1ad68674
VentGrey <omarpurataf@gmail.com>
2021-03-23 20:42:08 -0600

    Changes

--- UpVent/UpVent/settings.py
+++ UpVent/UpVent/settings.py
@@ -120,8 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Media files (Uploads, images, etc.)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
