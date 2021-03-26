6307a7a4c70f3ac0db78477d8093eb924ad9651c
VentGrey <omarpurataf@gmail.com>
2021-03-26 17:13:11 -0600

    various changes

--- UpVent/core/urls.py
+++ UpVent/core/urls.py
@@ -4,8 +4,11 @@ from django.conf import settings
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('services/', views.services, name='services'),
 ]
+
 
 # Media Root
 if settings.DEBUG:
--- UpVent/core/views.py
+++ UpVent/core/views.py
@@ -5,3 +5,9 @@ from django.shortcuts import render
 def index(request):
     index = ['01']
     return render(request, 'core/index.html', {'index': index})
+
+def about(request):
+    return render(request, 'core/about.html')
+
+def services(request):
+    return render(request, 'core/services.html')
