607ca700e5847256ff1879ee081fbca8e6780d57
VentGrey <omarpurataf@gmail.com>
2021-04-09 17:14:22 -0500

    Made project model more detailed

--- UpVent/core/models.py
+++ UpVent/core/models.py
@@ -7,21 +7,25 @@ class Project(models.Model):
     title = models.CharField(
         verbose_name="Título",
         max_length=200,
-        unique=True
+        unique=True,
+        help_text="Ingrese el nombre del proyecto"
     )
 
     image = models.ImageField(
         verbose_name="Imágen Destacada",
-        upload_to="portfolio"
+        upload_to="portfolio",
+        help_text="Se recomiendan imágenes de dimensiones rectangulares. Solo formatos jpg, jpeg, png y webm"
     )
 
     site = models.URLField(
         verbose_name="Sitio web",
         max_length=200,
+        help_text="El sitio web donde se encuentra alojado el proyecto"
     )
 
     description = RichTextUploadingField(
-        verbose_name="Descripción"
+        verbose_name="Descripción",
+        help_text="Descripción del proyecto. Procure ser breve."
     )
 
     created_on = models.DateTimeField(
@@ -34,6 +38,8 @@ class Project(models.Model):
 
     class Meta:
         ordering = ['created_on']
+        verbose_name = "Proyecto"
+        verbose_name_plural = "Proyectos"
 
     def __str__(self):
         return self.title
