97005be5f0cc24f1a7a9ea5a2ff08c0e5b71d827
VentGrey <omarpurataf@gmail.com>
2021-04-09 01:44:39 -0500

    Started writing portfolio model for About Us

--- UpVent/core/models.py
+++ UpVent/core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+# (about-us) Project model
+class Project(models.Model):
+
+    # Project tile
+    title = models.CharField(
+        verbose_name='Título',
+        max_length=200,
+        unique=True
+    )
+
+    # Project image
+    image = models.ImageField(
+        verbose_name='Imágen Destacada',
+        upload_to='portfolio'
+    )
