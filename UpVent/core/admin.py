6b8a087d486a9b6604670368acb462478c55673f
VentGrey <omarpurataf@gmail.com>
2021-04-09 04:32:02 -0500

    Added projects to admin manager

--- UpVent/core/admin.py
+++ UpVent/core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Project
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'site', 'created_on')
+    list_filter = ('title',)
+    search_fields = ['title', 'site']
+
+admin.site.register(Project, ProjectAdmin)
