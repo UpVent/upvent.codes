dcd08ee47376b0d70ce15946cb2dc84d375d6d1a
VentGrey <omarpurataf@gmail.com>
2021-04-15 02:16:57 -0500

    Added privacy policy to admin management

--- UpVent/core/admin.py
+++ UpVent/core/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Project
+from .models import Project, PrivacyPolicy
 
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('title', 'site', 'created_on')
     list_filter = ('title',)
     search_fields = ['title', 'site']
 
+class PolicyAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_on')
+
 admin.site.register(Project, ProjectAdmin)
+admin.site.register(PrivacyPolicy, PolicyAdmin)
