2d7d5ce9f15b172654b1bb8c5f5042e7ca73e615
VentGrey <omarpurataf@gmail.com>
2021-04-15 02:10:21 -0500

    Added a model for the privacy policy

--- UpVent/core/models.py
+++ UpVent/core/models.py
@@ -43,3 +43,43 @@ class Project(models.Model):
 
     def __str__(self):
         return self.title
+
+# (privacy-policy) Privacy Policy Project Model
+class PrivacyPolicy(models.Model):
+
+    title = models.CharField(
+        verbose_name="Título",
+        max_length=100,
+        help_text="Ingrese el título para su política de privacidad",
+        default="Política de Privacidad"
+    )
+
+    created_on = models.DateField(
+        auto_now_add=True
+    )
+
+    updated_on = models.DateField(
+        auto_now_add=True
+    )
+
+    changelog = models.TextField(
+        verbose_name="Registro de Cambios",
+        help_text="Ingrese la razón de cambiar la política de privacidad."
+    )
+
+    text = RichTextUploadingField(
+        verbose_name="Política de privacidad",
+        help_text="Escriba aquí su política de privacidad"
+    )
+
+    class Meta:
+        verbose_name = "Política de Privacidad"
+
+    def __str__(self):
+        return self.title
+
+    # Override 'save' method to allow only ONE instance for privacy policy
+    def save(self, *args, **kwargs):
+        if not self.pk and PrivacyPolicy.objects.exists():
+            raise ValidationError('You cannot have more than one privacy policy')
+        return super(PrivacyPolicy, self).save(*args, **kwargs)
