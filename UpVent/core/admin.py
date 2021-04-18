c554b87e23dfc9bd9c025af23b64455a4d8bd1dd
VentGrey <omarpurataf@gmail.com>
2021-04-17 21:34:13 -0500

    Registered FLOSS Project to admin

--- UpVent/core/admin.py
+++ UpVent/core/admin.py
@@ -1,15 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Project, PrivacyPolicy
+from .models import Project, FSProject, PrivacyPolicy
 
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('title', 'site', 'created_on')
     list_filter = ('title',)
     search_fields = ['title', 'site']
 
+class FSProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_on')
+    list_filter = ('title',)
+    search_fields = ['title', 'description', 'site', 'created_on']
+
 class PolicyAdmin(admin.ModelAdmin):
     list_display = ('title', 'created_on')
 
 admin.site.register(Project, ProjectAdmin)
+admin.site.register(FSProject, FSProjectAdmin)
 admin.site.register(PrivacyPolicy, PolicyAdmin)
