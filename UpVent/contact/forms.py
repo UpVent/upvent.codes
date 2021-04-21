0f40bf20ca3c83b1e8325b0628c7817d2e7b27d5
VentGrey <omarpurataf@gmail.com>
2021-04-21 18:02:10 -0500

    Pretty format forms.py

--- UpVent/contact/forms.py
+++ UpVent/contact/forms.py
@@ -16,11 +16,51 @@ class ContactForm(forms.ModelForm):
         ]
 
         widgets = {
-            'first_name': forms.TextInput(attrs={'class': 'autocomplete', 'id': 'first_name'}),
-            'last_name': forms.TextInput(attrs={'class': 'autocomplete', 'id': 'last_name'}),
-            'company_name': forms.TextInput(attrs={'class': 'autocomplete', 'id': 'company_name'}),
-            'email': forms.EmailInput(attrs={'class': 'autocomplete', 'id': 'email'}),
-            'phone': forms.TextInput(attrs={'class': 'autocomplete', 'id': 'company_name'}),
-            'reason': forms.TextInput(attrs={'class': 'autocomplete', 'id': 'first_name'}),
-            'message': forms.Textarea(attrs={'class': 'autocomplete', 'id': 'first_name'}),
+            'first_name': forms.TextInput(
+                attrs={
+                    'class': 'autocomplete',
+                    'id': 'first_name'
+                }
+            ),
+
+            'last_name': forms.TextInput(
+                attrs={
+                    'class': 'autocomplete',
+                    'id': 'last_name'
+                }
+            ),
+
+            'company_name': forms.TextInput(
+                attrs={
+                    'class': 'autocomplete',
+                    'id': 'company_name'
+                }
+            ),
+
+            'email': forms.EmailInput(
+                attrs={
+                    'class': 'autocomplete',
+                    'id': 'email'
+                }
+            ),
+
+            'phone': forms.TextInput(
+                attrs={
+                    'class': 'autocomplete',
+                    'id': 'company_name'
+                }
+            ),
+
+            'reason': forms.TextInput(
+                attrs={'class': 'autocomplete',
+                       'id': 'first_name'
+                       }
+            ),
+
+            'message': forms.Textarea(
+                attrs={
+                    'class': 'autocomplete',
+                    'id': 'first_name'
+                }
+            ),
         }
