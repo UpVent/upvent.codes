2f9721d5f0176fcddac65b328b0640d971ceb326
VentGrey <omarpurataf@gmail.com>
2021-04-21 18:02:19 -0500

    Register contacts in admin.py

--- UpVent/contact/admin.py
+++ UpVent/contact/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Contact
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email')
+    list_filter = ('first_name', 'email')
+    search_fields = ['first_name', 'last_name', 'email']
+
+admin.site.register(Contact, ContactAdmin)
