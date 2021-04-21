b1cbbcd3fae34b0e01643ce5a2e38e6c59b7390a
VentGrey <omarpurataf@gmail.com>
2021-04-20 19:42:43 -0500

    Added views.py file

--- UpVent/contact/views.py
+++ UpVent/contact/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import ContactForm
+from .models import Contact
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form = ContactForm
+            return render(request, 'contact/contact.html', {'form': form})
+    else:
+        form = ContactForm
+        return render(request, 'contact/contact.html', {'form': form})
