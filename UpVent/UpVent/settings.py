1ece2cc2f99c43fb933f7907fe29b53ef69985d0
VentGrey <omarpurataf@gmail.com>
2021-04-21 18:57:11 -0500

    Added generic user and password to django SMTP mail backend

--- UpVent/UpVent/settings.py
+++ UpVent/UpVent/settings.py
@@ -106,9 +106,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# Email Backends
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
+# Email Settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'toolreferenceinpassword@prog.com'
+EMAIL_HOST_PASSWORD = 'ThisPasswordIsRosettaStoned'
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
