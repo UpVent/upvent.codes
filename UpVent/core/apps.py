1cc40271dbea0fcb8f168957ae264dc8a618d8f9
VentGrey <omarpurataf@gmail.com>
2021-04-21 19:12:55 -0500

    Fix autofield warnings

--- UpVent/core/apps.py
+++ UpVent/core/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class CoreConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
     verbose_name = "Página Principal"
