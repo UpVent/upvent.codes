7c0b756c903d50e497ea8cb3007fb42cce143ecf
VentGrey <omarpurataf@gmail.com>
2021-04-23 18:44:43 -0500

    Added hcaptcha app to settings.py

--- UpVent/UpVent/settings.py
+++ UpVent/UpVent/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.admindocs',
     'admin_honeypot',
+    'hcaptcha',
     'ckeditor',
     'ckeditor_uploader',
     'core',
