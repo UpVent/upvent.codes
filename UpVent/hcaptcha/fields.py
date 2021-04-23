c9a1fdf5a2834afb628b5d49feb5a6e83e97a4b1
VentGrey <omarpurataf@gmail.com>
2021-04-23 18:29:53 -0500

    Populate fields.py

--- UpVent/hcaptcha/fields.py
+++ UpVent/hcaptcha/fields.py
@@ -1,28 +1,56 @@
+import json
+from urllib.error import HTTPError
 from urllib.parse import urlencode
+from urllib.request import build_opener, Request, ProxyHandler
 
 from django import forms
+from django.utils.translation import gettext_lazy as _
 
-from hcaptcha.settings import JS_API_URL, SITEKEY
+from hcaptcha.settings import DEFAULT_CONFIG, PROXIES, SECRET, TIMEOUT, VERIFY_URL
+from hcaptcha.widgets import hCaptchaWidget
 
 
-class hCaptchaWidget(forms.Widget):
-    template_name = 'hcaptcha_widget.html'
+class hCaptchaField(forms.Field):
+    widget = hCaptchaWidget
+    default_error_messages = {
+        'error_hcaptcha': _('hCaptcha could not be verified.'),
+        'invalid_hcaptcha': _('hCaptcha could not be verified.'),
+        'required': _('Please prove you are a human.'),
+    }
 
-    def __init__(self, *args, **kwargs):
-        self.extra_url = {}
-        super().__init__(*args, **kwargs)
+    def __init__(self, required=True, **config):
+        widget_settings = DEFAULT_CONFIG.copy()
+        widget_settings.update(config)
+        widget_url_settings = {}
+        for prop in filter(lambda p: p in widget_settings, ('onload', 'render', 'hl')):
+            widget_url_settings[prop] = widget_settings[prop]
+            del widget_settings[prop]
+        self.widget_settings = widget_settings
 
-    def value_from_datadict(self, data, files, name):
-        return data.get('h-captcha-response')
+        super().__init__(label='', help_text='', required=required)
 
-    def build_attrs(self, base_attrs, extra_attrs=None):
-        attrs = super().build_attrs(base_attrs, extra_attrs)
-        attrs['data-sitekey'] = SITEKEY
+        self.widget.extra_url = widget_url_settings
+
+    def widget_attrs(self, widget):
+        attrs = super().widget_attrs(widget)
+        for key, value in self.widget_settings.items():
+            attrs['data-%s' % key] = value
         return attrs
 
-    def get_context(self, name, value, attrs):
-        context = super().get_context(name, value, attrs)
-        context['api_url'] = JS_API_URL
-        if self.extra_url:
-            context['api_url'] += '?' + urlencode(self.extra_url)
-        return context
+    def validate(self, value):
+        super().validate(value)
+        opener = build_opener(ProxyHandler(PROXIES))
+        post_data = urlencode({
+            'secret': SECRET,
+            'response': value,
+        }).encode()
+        request = Request(VERIFY_URL, post_data)
+        try:
+            response = opener.open(request, timeout=TIMEOUT)
+        except HTTPError:
+            raise forms.ValidationError(self.error_messages['error_hcaptcha'], code='error_hcaptcha')
+
+        response_data = json.loads(response.read().decode("utf-8"))
+
+        if not response_data.get('success'):
+            raise forms.ValidationError(self.error_messages['invalid_hcaptcha'], code='invalid_hcaptcha')
