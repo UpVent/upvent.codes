cb4d158f0de415e24c93e78b40e25208494a0dcb
VentGrey <omarpurataf@gmail.com>
2021-05-03 19:55:16 -0500

    Working views.py

--- UpVent/contact/views.py
+++ UpVent/contact/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.core.mail import send_mail, BadHeaderError
+from django.contrib import messages
 from django.http import HttpResponse
+
+from django.conf import settings
 
 from .forms import ContactForm
 from .models import Contact
@@ -27,17 +30,28 @@ def contact(request):
                 send_mail(
                     subject,
                     message,
-                    'admin@example.com',
-                    ['admin@example.com']
+                    settings.EMAIL_HOST_USER,
+                    [form.cleaned_data['email']],
                 )
-            except BadHeaderError:
-                return HttpResponse('Invalid header found')
+                messages.success(request, 'Gracias por ponerse en contacto con\
+                nosotros. Responderemos a su mensaje en menos de 12 horas.')
 
+                # Return view
+                form.save()
+                form = ContactForm
+                return render(request, 'contact/contact.html', {'form': form})
 
-            # Return view
-            form = ContactForm
-            form.save()
+            except Exception as e:
+                messages.error(request, 'Algo salió mal,\
+                tu mensaje no fué enviado.')
+                return render(request, 'contact/contact.html', {'form': form})
+
+        else:
+            form = ContactForm(request.POST)
+            for field in form.errors:
+                form.fields[field].widget.attrs['class'] = 'form-control error'
             return render(request, 'contact/contact.html', {'form': form})
+
     else:
         form = ContactForm
         return render(request, 'contact/contact.html', {'form': form})
