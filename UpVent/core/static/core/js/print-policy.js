d8390cb9b3ee9bb31cd9ba76d8b5da3cb35a6c03
VentGrey <omarpurataf@gmail.com>
2021-05-14 17:03:08 -0500

    Added a printPolicy libreJS

---
+++ UpVent/core/static/core/js/print-policy.js
@@ -0,0 +1,12 @@
+// @license magnet:?xt=urn:btih:cf05388f2679ee054f2beb29a391d25f4e673ac3&dn=gpl-2.0.txt GPL-v2.0-or-Later
+function printDiv(divName) {
+    var printContents = document.getElementById(divName).innerHTML;
+    var originalContents = document.body.innerHTML;
+
+    document.body.innerHTML = printContents;
+
+    window.print();
+
+    document.body.innerHTML = originalContents;
+}
+// @license-end
