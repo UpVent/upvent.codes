98bccade5c4a56e0a5e239e4aa22a511dd27042e
VentGrey <omarpurataf@gmail.com>
2021-05-14 17:12:44 -0500

    Compressed print-policy JS

--- UpVent/core/static/core/js/print-policy.js
+++ UpVent/core/static/core/js/print-policy.js
@@ -1,12 +1,3 @@
 // @license magnet:?xt=urn:btih:cf05388f2679ee054f2beb29a391d25f4e673ac3&dn=gpl-2.0.txt GPL-v2.0-or-Later
-function printDiv(divName) {
-    var printContents = document.getElementById(divName).innerHTML;
-    var originalContents = document.body.innerHTML;
-
-    document.body.innerHTML = printContents;
-
-    window.print();
-
-    document.body.innerHTML = originalContents;
-}
+function printDiv(n){var e=document.getElementById(n).innerHTML,n=document.body.innerHTML;document.body.innerHTML=e,window.print(),document.body.innerHTML=n}
 // @license-end
