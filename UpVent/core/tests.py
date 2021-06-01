d905754ee11854936a8d90c0b63aa57743a8c171
VentGrey <omarpurataf@gmail.com>
2021-06-01 13:28:32 -0500

    Test user agents

--- UpVent/core/tests.py
+++ UpVent/core/tests.py
@@ -8,3 +8,11 @@ class RobotsTxtTests(TestCase):
         response = self.client.get("/robots.txt")
 
         self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response["content-type"], "text/plain")
+        lines = response.content.decode().splitlines()
+        self.assertEqual(lines[0], "User-Agent: *")
+
+    def test_post_disallowed(self):
+        response = self.client.post("/robots.txt")
+
+        self.assertEqual(HTTPStatus.METHOD_NOT_ALLOWED, response.status_code)
