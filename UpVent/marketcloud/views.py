175dabf527f083c9f7c5681c177ba37bbee49507
VentGrey <omarpurataf@gmail.com>
2021-06-01 01:08:45 -0500

    redirected things

--- UpVent/marketcloud/views.py
+++ UpVent/marketcloud/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def MarketCloud(request):
-    return render(request, 'marketcloud/shop.html', {})
+    return render(request, 'marketcloud/shop.html')
 
 def Product(request, slug):
-    return render(request, 'marketcloud/product.html', {})
+    return render(request, 'marketcloud/product.html')
