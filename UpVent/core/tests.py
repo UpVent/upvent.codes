ec59da3d04d1c7707aa5c0e6f2a95015423ce9b1
VentGrey <omarpurataf@gmail.com>
2021-06-01 13:29:58 -0500

    Ordered tests

--- UpVent/core/tests.py
+++ UpVent/core/tests.py
@@ -2,7 +2,6 @@ from http import HTTPStatus
 from django.test import TestCase
 
 # Robots.txt test
-
 class RobotsTxtTests(TestCase):
     def test_get(self):
         response = self.client.get("/robots.txt")
