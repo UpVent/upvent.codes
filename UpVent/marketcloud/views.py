9cd25c820fac535fd4aa5a9b9a3d06838e0adbeb
VentGrey <omarpurataf@gmail.com>
2021-06-04 02:14:06 -0500

    Added more stripe configurations to views.py file

--- UpVent/marketcloud/views.py
+++ UpVent/marketcloud/views.py
@@ -2,6 +2,7 @@ import stripe
 
 from django.conf import settings
 from django.shortcuts import render
+from django.http import JsonResponse
 from django.views import View
 
 # Import Stripe Keys
@@ -10,25 +11,29 @@ stripe.api_key = settings.STRIPE_SECRET_KEY
 
 class CreateCheckoutSessionView(View):
     def post(self, request, *args, **kwargs):
+        # Change this to your actual domain
+        YOUR_DOMAIN = "https://127.0.0.1:8000/"
         checkout_session = stripe.checkout.Session.create(
             payment_method_types=['card'],
             line_items=[
                 {
                     'price_data': {
-                        'currency': 'usd',
+                        'currency': 'mxn',
                         'unit_amount': 2000,
                         'product_data': {
                             'name': 'Stubborn Attachments',
-                            'images': ['https://i.imgur.com/EHyR2nP.png'],
                         },
                     },
                     'quantity': 1,
                 },
             ],
             mode='payment',
-            success_url=YOUR_DOMAIN + '/success.html',
-            cancel_url=YOUR_DOMAIN + '/cancel.html',
+            success_url=YOUR_DOMAIN + '/success/',
+            cancel_url=YOUR_DOMAIN + '/cancel/',
         )
+        return JsonResponse({
+            'id': checkout_session.id
+        })
 
 
 
