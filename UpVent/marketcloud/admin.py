aea1c599981657abe201f2311e488c3cb5ee4191
VentGrey <omarpurataf@gmail.com>
2021-06-04 04:05:12 -0500

    Improved product admin management

--- UpVent/marketcloud/admin.py
+++ UpVent/marketcloud/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Product
 
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'status', 'price', 'category', 'created_on')
+    list_filter = ("status",)
+    search_fields = ['name', 'price', 'category', 'created_on']
+    prepopulated_fields = { 'slug': ('name',) }
+
+admin.site.register(Product, ProductAdmin)
