8f8102947642e53f0e19d6d87c16cd7f5c6522a0
VentGrey <omarpurataf@gmail.com>
2021-06-07 14:43:00 -0500

    Added Terms Of Service Model

--- UpVent/marketcloud/models.py
+++ UpVent/marketcloud/models.py
@@ -100,3 +100,55 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+class TOS(models.Model):
+
+    """
+    Stores a single and UNIQUE terms of service + refund policy to make this
+    site compliant with inside / outside country trade laws.
+
+    This model has an overriden save() method to prevent more than one privacy
+    policy instance to be created.
+    """
+
+    title = models.CharField(
+        verbose_name="Título",
+        max_length=100,
+        help_text="Ingrese el título para sus términos y condiciones",
+        default="Términos y Condiciones + Política de Reembolsos",
+        blank=False
+    )
+
+    created_on = models.DateField(
+        verbose_name="Fecha de Creación",
+        auto_now_add=True
+    )
+
+    updated_on = models.DateField(
+        verbose_name="Fecha de Actualización",
+        auto_now_add=True
+    )
+
+    changelog = models.TextField(
+        verbose_name="Registro de Cambios",
+        help_text="Ingrese la razón de cambiar este documento.",
+        blank=False
+    )
+
+    text = RichTextUploadingField(
+        verbose_name="Términos del servicio",
+        help_text="Escriba aquí su política de reembolsos y términos"
+    )
+
+    class Meta:
+        verbose_name = "Términos del servicio"
+        verbose_name_plural = "Términos del servicio"
+
+    def __str__(self):
+        return self.title
+
+    # Override 'save' method to allow only ONE instance for TOS
+    def save(self, *args, **kwargs):
+        if not self.pk and TOS.objects.exists():
+            raise TOS('You cannot have more than one tos entry')
+        return super(TOS, self).save(*args, **kwargs)
