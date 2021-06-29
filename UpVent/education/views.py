201b844af5d54d4afcc567abd22034b22c8f6aa0
VentGrey <omarpurataf@gmail.com>
2021-06-29 18:16:50 -0500

    Added context to views

--- UpVent/education/views.py
+++ UpVent/education/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .models import Resource
 
 def Library(request):
 
-    return render(request, 'education/library.html', {})
+    books = Resource.objects.filter(category='Libro')
+    videos = Resource.objects.filter(category='Video')
+    other = Resource.objects.filter(category='Otro')
+
+    context = {
+        'books': books,
+        'videos': videos,
+        'other': other
+    }
+
+    return render(request, 'education/library.html', context)
