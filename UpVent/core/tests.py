2f842d98d88eeead917911b61bb77b6d085ae30d
VentGrey <omarpurataf@gmail.com>
2021-06-29 18:16:34 -0500

    Added some tests

--- UpVent/core/tests.py
+++ UpVent/core/tests.py
@@ -9,9 +9,17 @@ class RobotsTxtTests(TestCase):
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertEqual(response["content-type"], "text/plain")
         lines = response.content.decode().splitlines()
-        self.assertEqual(lines[0], "User-Agent: *")
+        self.assertEqual(lines[0], "Sitemap: https://upvent.codes/sitemap.xml")
+        self.assertEqual(lines[1], "User-Agent: *")
 
     def test_post_disallowed(self):
         response = self.client.post("/robots.txt")
 
         self.assertEqual(HTTPStatus.METHOD_NOT_ALLOWED, response.status_code)
+
+# sitemap.xml test
+class SitemapTests(TestCase):
+    def test_sitemap(self):
+        response = self.client.get("/sitemap.xml")
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
