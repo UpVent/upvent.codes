ee7362c5090e763e1b59708ee95b9cb41029fa40
VentGrey <omarpurataf@gmail.com>
2021-07-02 02:52:21 -0500

    Added libreJS magnet links

--- UpVent/core/static/core/js/darkmode.js
+++ UpVent/core/static/core/js/darkmode.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:cf05388f2679ee054f2beb29a391d25f4e673ac3&dn=gpl-2.0.txt GPL-v2.0-or-Later
 const toggle = document.querySelector("#toggledm");
 toggle.addEventListener("click", modeSwitch);
 
@@ -7,5 +8,10 @@ function modeSwitch() {
   isLight = !isLight;
   isLight ? toggle.innerText = "🌞" : toggle.innerText = "🌚";
   var rootElement = document.body;
+  /* Toggle dark body class in order to make body dark coloured */
   rootElement.classList.toggle("body-dark");
 }
+
+
+
+// @license-end
