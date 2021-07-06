a1dcccd760a547ba86f6eeed65961dc6e9022e32
VentGrey <omarpurataf@gmail.com>
2021-07-06 03:58:47 -0500

    Started adding some model tests in blog app

--- UpVent/blog/tests.py
+++ UpVent/blog/tests.py
@@ -1,3 +1,32 @@
+import tempfile
+from http import HTTPStatus
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-# Create your tests here.
+from blog.models import Post
+
+# Test blog urls
+class MainBlogPageTest(TestCase):
+    def test_home(self):
+        response = self.client.get("/blog/")
+        response301 = self.client.get("/about")
+        self.assertEqual(response301.status_code, HTTPStatus.MOVED_PERMANENTLY)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+
+# Test blog model creation
+
+class ModelCreation(TestCase):
+    def test_add_blog(self):
+        author = User(username="Test User")
+        newblog = Post()
+        newblog.title = "TestBlog"
+        newblog.STATUS = 1
+        newblog.description = "Test Description"
+        newblog.image = tempfile.NamedTemporaryFile(suffix='.jpg').name
+        newblog.category = "TestCat"
+        newblog.slug = "test-slug"
+        newblog.author = author
+        newblog.content = "Cosas"
+        newblog.updated_on
+        newblog.created_on
