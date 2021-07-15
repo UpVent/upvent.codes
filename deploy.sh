4fd07dd664a224284d7315fb7e02afbc0826939f
VentGrey <omarpurataf@gmail.com>
2021-07-15 03:02:58 -0500

    Add license notice

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+    # This program is free software: you can redistribute it and/or modify
+    # it under the terms of the GNU General Public License as published by
+    # the Free Software Foundation, verion 2 ONLY.
+
+    # This program is distributed in the hope that it will be useful,
+    # but WITHOUT ANY WARRANTY; without even the implied warranty of
+    # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    # GNU General Public License for more details.
+
+    # You should have received a copy of the GNU General Public License
+    # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 # ==== Check user and environment ====
 function check_user() {
     if [[ $EUID -ne 0 ]]; then
