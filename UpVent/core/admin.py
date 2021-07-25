1bd779463ea300d52494a376340e79b5bcc427de
VentGrey <omarpurataf@gmail.com>
2021-07-25 01:01:19 -0500

    Added empty value display to core-admin

--- UpVent/core/admin.py
+++ UpVent/core/admin.py
@@ -14,37 +14,45 @@ class TestimonialAdmin(admin.ModelAdmin):
     list_display = ('name', 'workplace', 'site')
     list_filter = ('name', 'workplace')
     search_fields = ['name', 'workplace', 'site']
+    empty_value_display = '-vacío-'
 
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('title', 'site', 'created_on')
     list_filter = ('title',)
     search_fields = ['title', 'site']
+    empty_value_display = '-vacío-'
 
 class FSProjectAdmin(admin.ModelAdmin):
     list_display = ('title', 'plicense', 'github_addr', 'created_on',
                     'updated_on')
     list_filter = ('title', 'plicense')
     search_fields = ['title', 'description', 'github_addr', 'created_on']
+    empty_value_display = '-vacío-'
 
 class LicenseAdmin(admin.ModelAdmin):
     list_display = ('name', 'license_link')
     list_filter = ('name',)
     search_fields = ['name', 'license_link']
+    empty_value_display = '-vacío-'
 
 class HOFAdmin(admin.ModelAdmin):
     list_display = ('name',)
     search_fields = ['name', ]
+    empty_value_display = '-vacío-'
 
 class TeamMemberAdmin(admin.ModelAdmin):
     list_display = ('name', 'position')
     list_filter = ('name', 'position')
     search_fields = ['name', 'position']
+    empty_value_display = '-vacío-'
 
 class PolicyAdmin(admin.ModelAdmin):
     list_display = ('title', 'created_on')
+    empty_value_display = '-vacío-'
 
 class TOSAdmin(admin.ModelAdmin):
     list_display = ('title', 'created_on')
+    empty_value_display = '-vacío-'
 
 admin.site.register(Testimonial, TestimonialAdmin)
 admin.site.register(Project, ProjectAdmin)
