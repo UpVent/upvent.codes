d247b21b75657f65d58f6f98051998234eb56082
VentGrey <omarpurataf@gmail.com>
2021-08-17 03:10:11 -0500

    Added darkmode-js

---
+++ UpVent/core/static/core/js/darkmode-config.js
@@ -0,0 +1,13 @@
+function addDarkmodeWidget() {
+
+  const options = {
+    time: '0.5s',
+    label: '🌞',
+    saveInCookies: false,
+    autoMatchOsTheme: true
+  }
+
+  new Darkmode(options).showWidget();
+}
+
+window.addEventListener('load', addDarkmodeWidget);
