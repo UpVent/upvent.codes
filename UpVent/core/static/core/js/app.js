165fb30b9cac9eda1fa5a7cef84d2654dcb9c345
VentGrey <omarpurataf@gmail.com>
2021-09-13 03:31:52 -0500

    Corrected modules

--- UpVent/core/static/core/js/app.js
+++ UpVent/core/static/core/js/app.js
@@ -1,1 +1,10 @@
-import { h, Component, render } from 'preact.min.js'
+import { h, render, Component } from './preact.min.js'
+import htm from './htm.min.js'
+
+const html = htm.bind(h);
+
+function App (props) {
+    return html`<h1>Hello ${props.name}!</h1>`;
+  }
+
+  render(html`<${App} name="World" />`, document.getElementById("coso"));
