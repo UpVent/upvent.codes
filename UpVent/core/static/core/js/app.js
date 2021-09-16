7edf6d0fa126be46f157ef726d6d65003ffa7879
VentGrey <omarpurataf@gmail.com>
2021-09-15 23:22:21 -0500

    fully working preact + htm

--- UpVent/core/static/core/js/app.js
+++ UpVent/core/static/core/js/app.js
@@ -3,8 +3,18 @@ import htm from './htm.min.js'
 
 const html = htm.bind(h);
 
-function App (props) {
-    return html`<h1>Hello ${props.name}!</h1>`;
-  }
+/**
+ * This is the main App class for our index.html file
+ * */
+class App extends Component {
 
-  render(html`<${App} name="World" />`, document.getElementById("coso"));
+    /**
+     * Render this component into readable HTML
+     * @returns {html} The readable html elements for this page.
+     * */
+    render() {
+        return html `<h1>Hello, world!</h1>`;
+    }
+}
+
+render(html`<${App}/>`, document.getElementById("coso"));
